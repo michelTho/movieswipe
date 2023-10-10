8fe2bc166bb752ce69f0af80b282ff37e6e80f8f
Michel Thomazo <thomazo.michel@gmail.com>
2023-10-11 00:40:27 +0200

    initial commit

---
+++ front/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/static/src/vue/dist/', // Should be STATIC_URL + path/to/build
+  outputDir: path.resolve(__dirname, '../static/src/vue/dist/'), // Output to a directory in STATICFILES_DIRS
+  filenameHashing: false, // Django will hash file names, not webpack
+  runtimeCompiler: true, // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+  devServer: {
+    devMiddleware: {
+      // see https://github.com/webpack/webpack-dev-server/issues/2958
+      writeToDisk: true, 
+    }
+  },
+})
