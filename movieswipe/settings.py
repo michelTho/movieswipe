df82deff4b974498113567492de4bd721f72e8cf
Michel Thomazo <thomazo.michel@gmail.com>
2023-10-11 01:18:15 +0200

    wip

--- movieswipe/settings.py
+++ movieswipe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c#(o34o1=1c(kr=9b)vzr7ey!*b8i86+qi*2i@_t)ayu$j=iw_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
@@ -138,4 +138,4 @@ STORAGES = {
     },
 }
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = os.environ.getenv("ALLOWED_HOSTS", ["*"])
