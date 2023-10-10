f7ad6d0fb87c9b24aec4e81b998bb6ccb1034d5c
Michel Thomazo <thomazo.michel@gmail.com>
2023-10-11 00:56:41 +0200

    wip

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 from django.core.management.commands.runserver import Command as runserver
 
-runserver.default_port = os.getenv("PORT", "8000")
+runserver.default_port = os.getenv("PORT")
 
 
 def main():
