4ce53cf3be9fc6567d0aec8f9d6feee4d714aa59
Michel Thomazo <thomazo.michel@gmail.com>
2023-10-22 01:14:02 +0200

    try to fix pb internal server error

--- movieswipe/settings.py
+++ movieswipe/settings.py
@@ -32,7 +32,7 @@ if "ENV" not in os.environ:
 SECRET_KEY = "django-insecure-c#(o34o1=1c(kr=9b)vzr7ey!*b8i86+qi*2i@_t)ayu$j=iw_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = False  # os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "movieswipe.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         conn_health_checks=True,
-        ssl_require=os.environ.get("SSL_REQUIRE") == "True",
+        ssl_require=True,  # os.environ.get("SSL_REQUIRE") == "True",
     ),
 }
 
